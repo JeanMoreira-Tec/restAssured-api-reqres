67ac2ff7fef0484f0548ad4e6941b0f5399e9576
Jean Moreira <jean.biomedicina@gmail.com>
2023-03-17 08:51:41 -0300

    Adiciona teste de falha de lgoin

--- app/src/test/java/teste/TesteRegistro.java
+++ app/src/test/java/teste/TesteRegistro.java
@@ -1,7 +1,11 @@
 package teste;
 
 import dominio.Usuario;
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,6 +14,14 @@ import static org.hamcrest.CoreMatchers.is;
 public class TesteRegistro extends TesteBase {
 
     private static final String REGISTRA_USUARIO_ENDPOINT = "/register";
+    private static final String LOGIN_USUARIO_ENDPOINT = "/login";
+
+    @BeforeClass
+    public static void setupRegistro() {
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .build();
+    }
 
     @Test
     public void testeNaoEfetuaRegistroQuandoSenhaEstaFaltando() {
@@ -22,5 +34,18 @@ public class TesteRegistro extends TesteBase {
         then().
             statusCode(HttpStatus.SC_BAD_REQUEST).
             body("error", is("Missing password"));
+    }
+
+    @Test
+    public void testeNaoEfetuadoLoginQuandoSenhaEstaFaltando() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("sydney@fife");
+        given().
+                body(usuario).
+                when().
+                post(LOGIN_USUARIO_ENDPOINT).
+                then().
+                statusCode(HttpStatus.SC_BAD_REQUEST).
+                body("error", is("Missing password"));
     }
 }
